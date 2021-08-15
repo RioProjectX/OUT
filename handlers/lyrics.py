acbaffeb7a418f8d8ccff02d6175f005ab00d1db
Kennedy <abdulmutaqin14@gmail.com>
2021-08-15 13:33:04 +0700

    Create lyrics.py

---
+++ handlers/lyrics.py
@@ -0,0 +1,22 @@
+# Module by https://github.com/tofikdn
+# Copyright (C) 2021 TdMusic
+
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+
+@Client.on_message(command(["lyric", f"lyric@{BOT_USERNAME}"]))
+async def lirik(_, message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("**give a lyric name too !**")
+            return
+        query = message.text.split(None, 1)[1]
+        rep = await message.reply_text("🔎 **searching lyrics...**")
+        resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
+        result = f"{resp['data']}"
+        await rep.edit(result)
+    except Exception:
+        await rep.edit("**Lyrics not found.** please give a valid song name !")
