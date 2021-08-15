d5d6ad9329455c4d23870d3555d6f38d19c7be12
Kennedy <abdulmutaqin14@gmail.com>
2021-08-15 18:15:48 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -96,7 +96,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("etc/Roboto-Medium.ttf", 45)
-    draw.text((20, 630), f"Playing here...", (0, 59, 78), font=font)
+    draw.text((20, 630), f"Now playing...", (0, 67, 82), font=font)
     draw.text((20, 670),
         f"{title[:25]}...",
         (0, 59, 78),
