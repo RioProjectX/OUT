a9cfb123807894cd7d808b12cf4ee9ff0b23b888
Kennedy <abdulmutaqin14@gmail.com>
2021-08-15 15:09:34 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -2,7 +2,7 @@ from asyncio import QueueEmpty
 from config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from cache.admins import set
 from helpers.channelmusic import get_chat_id
