eaa92d5c113976cc40a2ea247c3d459ec44796cb
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-09-01 00:02:58 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -23,6 +23,20 @@ async def update_admin(client, message):
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
     await message.reply_text("✅ Bot **berhasil dimuat ulang !**\n✅ **Daftar admin** telah **diperbarui !**")
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Group Support", url=f"https://t.me/siiniaja"
+                    ),
+                    InlineKeyboardButton(
+                        "Project By", url=f"https://t.me/riio00"
+                    )
+                ]
+            ]
+        )
+   )
+
 
 
 @Client.on_message(command("pause") & other_filters)
